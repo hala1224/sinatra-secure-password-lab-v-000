ea2560cb7573b972df65f85f2b3e47a7b7e535d3
Natalie Halawani <hala1224@gmail.com>
2019-03-11 02:56:21 +0000

    Automatically backed up by Learn

--- db/migrate/20190311025537_create_users.rb
+++ db/migrate/20190311025537_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    
+    def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.decimal :balance
+    end
     
     
   end
