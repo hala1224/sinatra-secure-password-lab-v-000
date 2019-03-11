bc907f25e821828d5a5defccdeb801723e99c9cd
Natalie Halawani <hala1224@gmail.com>
2019-03-11 02:58:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190311002932) do
+ActiveRecord::Schema.define(version: 20190311025537) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string  "username"
+    t.string  "password_digest"
+    t.decimal "balance"
   end
 
 end
